1ee43ba1a0ec20d13ac31b275dcaa9d13ea0b428
Conrad Kleinespel <conradk@conradk.com>
2024-09-14 07:23:57 +0200

    Rename test file

--- libm8s/tests/test_file_format.rs
+++ libm8s/tests/test_file_format.rs
@@ -2,7 +2,7 @@ use libm8s::file_format::{Config, Unit, UnitWithDependencies};
 
 #[test]
 fn test_parse_only_units() {
-    let test_file_yaml = include_str!("file_most_basic.yaml");
+    let test_file_yaml = include_str!("m8s_only_units.yaml");
 
     let config: Config = serde_yaml::from_str(test_file_yaml).unwrap();
     assert_eq!(
