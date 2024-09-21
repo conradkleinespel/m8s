c9cef65d7bca1d6f27cbc016066e9260d6927842
Conrad Kleinespel <conradk@conradk.com>
2024-09-21 10:24:59 +0200

    Show command output live instead of after the fact

--- libm8s/src/utils.rs
+++ libm8s/src/utils.rs
@@ -2,6 +2,7 @@ use log::debug;
 use std::io;
 use std::io::{BufRead, BufReader};
 use std::process::{Command, Stdio};
+use std::sync::{Arc, Mutex};
 
 pub(crate) fn run_command_with_piped_stdio(
     program: &str,
@@ -28,27 +29,46 @@ pub(crate) fn run_command_with_piped_stdio(
     let mut child = command.spawn().expect("Failed to execute command");
 
     let stdout = child.stdout.take().expect("Failed to capture stdout");
-    let reader = BufReader::new(stdout);
-    let mut output_stdout = String::new();
-    for line in reader.lines() {
-        let line = line?;
-        output_stdout.push_str(&line);
-        output_stdout.push('\n');
-        println!("{}", line);
-    }
-
     let stderr = child.stderr.take().expect("Failed to capture stderr");
-    let reader = BufReader::new(stderr);
-    let mut output_stderr = String::new();
-    for line in reader.lines() {
-        let line = line?;
-        output_stderr.push_str(&line);
-        output_stderr.push('\n');
-        println!("{}", line);
-    }
+
+    let output_stdout = Arc::new(Mutex::new(String::new()));
+    let output_stderr = Arc::new(Mutex::new(String::new()));
+
+    let stdout_reader = BufReader::new(stdout);
+    let stderr_reader = BufReader::new(stderr);
+
+    let stdout_handle = {
+        let output_stdout = Arc::clone(&output_stdout);
+        std::thread::spawn(move || {
+            for line in stdout_reader.lines() {
+                let line = line.expect("Failed to read line from stdout");
+                println!("{}", line);
+                output_stdout.lock().unwrap().push_str(&line);
+                output_stdout.lock().unwrap().push('\n');
+            }
+        })
+    };
+
+    let stderr_handle = {
+        let output_stderr = Arc::clone(&output_stderr);
+        std::thread::spawn(move || {
+            for line in stderr_reader.lines() {
+                let line = line.expect("Failed to read line from stderr");
+                eprintln!("{}", line);
+                output_stderr.lock().unwrap().push_str(&line);
+                output_stderr.lock().unwrap().push('\n');
+            }
+        })
+    };
+
+    stdout_handle.join().expect("Failed to join stdout thread");
+    stderr_handle.join().expect("Failed to join stderr thread");
 
     if !child.wait()?.success() {
-        return Err(io::Error::new(io::ErrorKind::Other, output_stderr));
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            output_stderr.lock().unwrap().to_string(),
+        ));
     }
     Ok(())
 }
